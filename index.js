fc409686d5ecb434dedd28029b71b9cbfc5de98d
tashiq <tashiq>
2021-11-17 22:16:07 +0600

    basic setup

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const { MongoClient } = require('mongodb');
 const app = express();
-app.use(express.json());
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
 app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elbg4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const ObjectId = require('mongodb').ObjectId;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function server() {
+    try {
+        await client.connect();
+        const database = client.db('assignment12');
+        const productsCollection = database.collection('products');
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const objId = ObjectId(id);
+            const query = {
+                _id: objId
+            }
+            const result = await productsCollection.findOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+
+    }
+}
+server().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+app.listen(port, () => {
+    console.log(port);
+})
