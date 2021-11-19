787aaac375c907ed8c5ed0038979d7f7df65c0e1
tashiq <tashiq>
2021-11-19 12:28:29 +0600

    all done

--- index.js
+++ index.js
@@ -16,10 +16,16 @@ async function server() {
         const productsCollection = database.collection('products');
         const reviewCollection = database.collection('reviews');
         const orderCollection = database.collection('orders');
+        const userCollection = database.collection('users');
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const result = await cursor.toArray();
             res.json(result);
+        })
+        app.post('/products', async (req, res) => {
+            const data = req.body;
+            const result = await productsCollection.insertOne(data);
+            res.json(result)
         })
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
@@ -29,6 +35,16 @@ async function server() {
             }
             const result = await productsCollection.findOne(query);
             res.json(result);
+        })
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const objId = ObjectId(id);
+            const query = {
+                _id: objId
+            }
+            const result = await productsCollection.deleteOne(query);
+
+            res.json(result)
         })
         app.post('/reviews', async (req, res) => {
             const data = req.body;
@@ -40,11 +56,61 @@ async function server() {
             const result = await cursor.toArray();
             res.json(result)
         })
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const result = await cursor.toArray();
+            res.json(result)
+        })
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const result = await cursor.toArray();
+            res.json(result)
+        })
+
         app.post('/orders', async (req, res) => {
             const data = req.body;
             const result = await orderCollection.insertOne(data);
-            console.log(result);
             res.json(result)
+        })
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            }
+            const updateDoc = {
+                $set: {
+                    shipped: true
+                }
+            }
+            const result = await orderCollection.updateOne(query, updateDoc)
+            res.json(result)
+        })
+        app.post('/users', async (req, res) => {
+            const data = req.body;
+            const result = await userCollection.insertOne(data);
+            res.json(result);
+        })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {
+                email: email
+            }
+            const result = await userCollection.findOne(query);
+            res.json(result);
+        })
+        app.put('/admin', async (req, res) => {
+            const email = req.query.email;
+            const filter = { email: email }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.json(result);
         })
     }
     finally {
